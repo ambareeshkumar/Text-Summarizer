806e3efa16ff0f50812f453a1de0c79590b6bbc4
ambareeshkumar <ambareeshkumar10@gmail.com>
2024-06-12 17:52:27 +0530

    Added Model Trainer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from TextSummarizer.pipeline.DataIngestion_Pipeline import DataIngestion_Pipeline
 from TextSummarizer.pipeline.DataValidation_Pipeline import DataValidationPipeline
-from TextSummarizer.pipeline.DataTransformationPipeline import DataTransformationPipeline
-from TextSummarizer.pipeline.ModelTrainerPipeline import ModelTrainerPipeline
+from TextSummarizer.pipeline.DataTransformation_Pipeline import DataTransformationPipeline
+from TextSummarizer.pipeline.ModelTrainer_Pipeline import ModelTrainerPipeline
+from TextSummarizer.pipeline.ModelEvaluation_Pipeline import ModelEvaluationPipeline
 
 from TextSummarizer.logging import logging
 
@@ -52,3 +53,14 @@ except Exception as e:
     logging.error(f"-------------- {STAGE_NAME} Failed --------------")
     raise e
 
+STAGE_NAME = "Model Evaluation Pipeline"
+
+try:
+    logging.info(f"-------------- {STAGE_NAME} Started --------------")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
+    logging.info(f"-------------- {STAGE_NAME} Completed --------------")
+
+except Exception as e:
+    logging.error(f"-------------- {STAGE_NAME} Failed --------------")
+    raise e
